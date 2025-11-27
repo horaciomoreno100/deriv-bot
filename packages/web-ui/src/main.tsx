44c8d146221ebbf07e736c2a2d44bc6766ea10cf
Horacio Moreno <hmoreno@weareappa.com>
2025-11-27 20:55:10 -0300

    chore: deploy v0.4.0 (20251127_205507)

--- packages/web-ui/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-// Note: StrictMode is disabled because it causes issues with WebSocket connections
-// in development (double-mounting closes the connection prematurely)
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
-);
